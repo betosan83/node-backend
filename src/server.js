32f5ea654c9e11b277df6dcc54be66e602b5ced2
Roberto Boscolo <beto.boscolo@gmail.com>
2019-08-08 20:01:02 -0300

    Nova versão do aplicativo. Finalizado o backend da aplicação.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
+
 
 const server = express();
 
-mongoose.connect('mongodb+srv://betosan:betosan@cluster0-qj4a4.mongodb.net/omnistack8?retryWrites=true&w=majority', 
+mongoose.connect('mongodb+srv://betosan:betosan@cluster0-qj4a4.mongodb.net/test?retryWrites=true&w=majority', 
 {useNewUrlParser: true});
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
